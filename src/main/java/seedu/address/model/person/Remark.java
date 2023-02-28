ca84b38ce86c0fed178d90624cc9b6bd39c66f04
zxisatree <62459441+zxisatree@users.noreply.github.com>
2023-02-28 15:18:09 +0800

    Fix checkstyle problems

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a new remark.
+     *
+     * @param remark The remark to wrap.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
